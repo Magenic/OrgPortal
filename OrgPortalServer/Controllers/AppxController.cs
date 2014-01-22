a23851b8802c72f87ee59f16b6fbcc001ca5a39f
dpdonahue <dpdonahue@gmail.com>
2014-01-22 12:18:23 -0500

    Added some structure and TODO placeholders for APPX file upload functionality.

---
+++ OrgPortalServer/Controllers/AppxController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using OrgPortalServer.Models;
+using System.Net.Http.Headers;
+using System.Web;
+using System.IO;
+
+namespace OrgPortalServer.Controllers
+{
+    public class AppxController : ApiController
+    {
+        // GET api/<controller>/5
+        public HttpResponseMessage Get(int id)
+        {
+            // TODO: Get the persisted appx file
+            var result = new HttpResponseMessage(HttpStatusCode.OK);
+            // TODO: Should the content type be different?
+            result.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+            // TODO: Set result.Content to a stream of the file data
+            // TODO: Set any more headers?  Content-Length, perhaps?
+            return result;
+        }
+
+        // POST api/<controller>
+        public void Post(HttpPostedFileBase postedFile)
+        {
+            if (postedFile.ContentLength > 0)
+            {
+                var stream = new MemoryStream();
+                postedFile.InputStream.CopyTo(stream);
+                var appxFile = new AppxFile(stream.ToArray());
+                // TODO: Is there anything else to do here?
+                appxFile.Save();
+            }
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id)
+        {
+            // TODO: Is there any support for this?  Will we over-write an existing app by uploading a new appx file?
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+            // TODO: Is there any support for this?
+        }
+    }
+}
---
+++ OrgPortalServer/Models/AppxFile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrgPortalServer.Models
+{
+    public class AppxFile
+    {
+        public byte[] Data { get; private set; }
+
+        private AppxFile() { }
+
+        public AppxFile(byte[] data)
+        {
+            Data = data;
+            // TODO: Do we need to know anything else here?  Content-Type?  I don't think so.
+
+            // TODO: Validate that the byte array is an APPX file, extract other fields from it
+        }
+
+        public void Save()
+        {
+            // TODO: Persist the model to the backing data store
+        }
+    }
+}
