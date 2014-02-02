0f271bfb09199386d26e99615959498b0a272e2f
dpdonahue <dpdonahue@gmail.com>
2014-02-02 14:53:45 -0500

    Began extracting some values from the APPX file.  We may only need to persist the stream/bytes of the APPX itself and just let the model handle the extraction of values at runtime.  So at least for now I may start persisting these to the file system just to get some back-end persistence going.

--- OrgPortalServer/Controllers/AppxController.cs
+++ OrgPortalServer/Controllers/AppxController.cs
@@ -8,6 +8,8 @@ using OrgPortalServer.Models;
 using System.Net.Http.Headers;
 using System.Web;
 using System.IO;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace OrgPortalServer.Controllers
 {
@@ -18,49 +20,54 @@ namespace OrgPortalServer.Controllers
         {
             var appxFile = AppxFile.Get(id);
             var response = new HttpResponseMessage(HttpStatusCode.OK);
-            response.Content = new StreamContent(new MemoryStream(appxFile.Data));
+            response.Content = new StreamContent(new MemoryStream(appxFile.Package));
             response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
-            response.Content.Headers.ContentLength = appxFile.Data.Length;
+            response.Content.Headers.ContentLength = appxFile.Package.Length;
             return response;
         }
 
         // POST api/<controller>
         public void Post(HttpRequestMessage request)
         {
-            var multiPartContents = request.Content.ReadAsMultipartAsync();
-            multiPartContents.Wait();
-            if (multiPartContents.Result.Contents.Count > 0)
-            {
-                var stream = multiPartContents.Result.Contents.First().ReadAsStreamAsync();
-                stream.Wait();
-                AppxFile.Get(stream.Result).Save();
-            }
+            var stream = GetStreamFromUploadedFile(request);
+            AppxFile.Get(stream).Save();
         }
 
         // PUT api/<controller>
         public void Put(HttpRequestMessage request)
         {
-            var multiPartContents = request.Content.ReadAsMultipartAsync();
-            multiPartContents.Wait();
-            if (multiPartContents.Result.Contents.Count > 0)
-            {
-                var stream = multiPartContents.Result.Contents.First().ReadAsStreamAsync();
-                stream.Wait();
-                AppxFile.Get(stream.Result).Save();
-            }
+            var stream = GetStreamFromUploadedFile(request);
+            AppxFile.Get(stream).Save();
         }
 
         // DELETE api/<controller>
         public void Delete(HttpRequestMessage request)
         {
-            var multiPartContents = request.Content.ReadAsMultipartAsync();
-            multiPartContents.Wait();
-            if (multiPartContents.Result.Contents.Count > 0)
-            {
-                var stream = multiPartContents.Result.Contents.First().ReadAsStreamAsync();
-                stream.Wait();
-                AppxFile.Get(stream.Result).Delete();
-            }
+            var stream = GetStreamFromUploadedFile(request);
+            AppxFile.Get(stream).Delete();
+        }
+
+        private static Stream GetStreamFromUploadedFile(HttpRequestMessage request)
+        {
+            // Awaiting these tasks in the usual manner was deadlocking the thread for some reason.
+            // So for now we're invoking a Task and explicitly creating a new thread.
+            // See here: http://stackoverflow.com/q/15201255/328193
+            IEnumerable<HttpContent> parts = null;
+            Task.Factory
+                .StartNew(() => parts = request.Content.ReadAsMultipartAsync().Result.Contents,
+                                CancellationToken.None,
+                                TaskCreationOptions.LongRunning,
+                                TaskScheduler.Default)
+                .Wait();
+
+            Stream stream = null;
+            Task.Factory
+                .StartNew(() => stream = parts.First().ReadAsStreamAsync().Result,
+                                CancellationToken.None,
+                                TaskCreationOptions.LongRunning,
+                                TaskScheduler.Default)
+                .Wait();
+            return stream;
         }
     }
 }
--- OrgPortalServer/Models/AppxFile.cs
+++ OrgPortalServer/Models/AppxFile.cs
@@ -4,21 +4,29 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Web;
+using System.Xml.Linq;
 
 namespace OrgPortalServer.Models
 {
     public class AppxFile
     {
-        public byte[] Data { get; private set; }
+        public byte[] Package { get; private set; }
+        public string Name { get; private set; }
+        public string Publisher { get; private set; }
+        public string Version { get; private set; }
+        public string ProcessorArchitecture { get; private set; }
+        public string DisplayName { get; private set; }
+        public string PublisherDisplayName { get; private set; }
+        public byte[] Logo { get; private set; }
+        public byte[] SmallLogo { get; private set; }
 
         private AppxFile() { }
 
         private AppxFile(Stream data)
         {
-            // TODO: Validate that the byte array is an APPX file, extract other fields from it, essentially build the application and meta-data from the file.
-            
-            // TODO: This is failing to read the stream.  Maybe the upload is being corrupted in the controller somehow?
-            var zip = ZipFile.Read(data);
+            ExtractValuesFromPackage(data);
+            data.Seek(0, SeekOrigin.Begin);
+            Package = ReadFully(data);
         }
 
         public void Save()
@@ -46,6 +54,117 @@ namespace OrgPortalServer.Models
             // TODO: Might not be a string, determine the unique identifier from the APPX when we've parsed that.
             // TODO: Get the APPX and related data from the backing data store
             return new AppxFile();
+        }
+
+        private void ExtractValuesFromPackage(Stream data)
+        {
+            using (var zipArchive = ZipFile.Read(data))
+            {
+                var manifest = GetManifestFromZip(zipArchive);
+                ExtractName(manifest);
+                ExtractPublisher(manifest);
+                ExtractVersion(manifest);
+                ExtractProcessorArchitecture(manifest);
+                ExtractDisplayName(manifest);
+                ExtractPublisherDisplayName(manifest);
+                ExtractLogo(zipArchive, manifest);
+                ExtractSmallLogo(zipArchive, manifest);
+            }
+        }
+
+        private static XDocument GetManifestFromZip(ZipFile zipArchive)
+        {
+            // Juggling streams because they were closed when I accessed them before
+            // See here: http://stackoverflow.com/q/8100590/328193
+            // This can probably be made more efficient
+            var manifestFile = zipArchive.Entries.Single(e => e.FileName == "AppxManifest.xml");
+            var manifestData = new MemoryStream();
+            manifestFile.Extract(manifestData);
+            var manifest = XDocument.Load(new MemoryStream(manifestData.ToArray()));
+            return manifest;
+        }
+
+        private void ExtractName(XDocument manifest)
+        {
+            Name = manifest.Descendants().Single(d => d.Name.LocalName == "Identity")
+                           .Attributes().Single(a => a.Name.LocalName == "Name")
+                           .Value;
+        }
+
+        private void ExtractPublisher(XDocument manifest)
+        {
+            Publisher = manifest.Descendants().Single(d => d.Name.LocalName == "Identity")
+                                .Attributes().Single(a => a.Name.LocalName == "Publisher")
+                                .Value;
+        }
+
+        private void ExtractVersion(XDocument manifest)
+        {
+            Version = manifest.Descendants().Single(d => d.Name.LocalName == "Identity")
+                              .Attributes().Single(a => a.Name.LocalName == "Version")
+                              .Value;
+        }
+
+        private void ExtractDisplayName(XDocument manifest)
+        {
+            DisplayName = manifest.Descendants().Single(d => d.Name.LocalName == "Properties")
+                                  .Descendants().Single(d => d.Name.LocalName == "DisplayName")
+                                  .Value;
+        }
+
+        private void ExtractPublisherDisplayName(XDocument manifest)
+        {
+            PublisherDisplayName = manifest.Descendants().Single(d => d.Name.LocalName == "Properties")
+                                           .Descendants().Single(d => d.Name.LocalName == "PublisherDisplayName")
+                                           .Value;
+        }
+
+        private void ExtractProcessorArchitecture(XDocument manifest)
+        {
+            ProcessorArchitecture = manifest.Descendants().Single(d => d.Name.LocalName == "Identity")
+                                            .Attributes().Single(a => a.Name.LocalName == "ProcessorArchitecture")
+                                            .Value;
+        }
+
+        private void ExtractLogo(ZipFile zipArchive, XDocument manifest)
+        {
+            var logoData = ExtractAssetImageFromVisualElementsNode(zipArchive, manifest, "Square150x150Logo");
+            Logo = ReadFully(new MemoryStream(logoData.ToArray()));
+        }
+
+        private void ExtractSmallLogo(ZipFile zipArchive, XDocument manifest)
+        {
+            var logoData = ExtractAssetImageFromVisualElementsNode(zipArchive, manifest, "Square30x30Logo");
+            SmallLogo = ReadFully(new MemoryStream(logoData.ToArray()));
+        }
+
+        private static MemoryStream ExtractAssetImageFromVisualElementsNode(ZipFile zipArchive, XDocument manifest, string attributeName)
+        {
+            var logoFileName = manifest.Descendants().Single(d => d.Name.LocalName == "Applications")
+                                       .Descendants().First(d => d.Name.LocalName == "Application") // TODO: What if there is more than one application?
+                                       .Descendants().Single(d => d.Name.ToString().EndsWith("VisualElements", StringComparison.InvariantCultureIgnoreCase))
+                                       .Attributes().Single(a => a.Name.LocalName == attributeName)
+                                       .Value;
+            var fileName = Path.GetFileNameWithoutExtension(logoFileName);
+            var fileExtension = Path.GetExtension(logoFileName);
+            var logoFile = zipArchive.Entries.Single(e => Path.GetFileName(e.FileName).StartsWith(fileName, StringComparison.InvariantCultureIgnoreCase) &&
+                                                          Path.GetExtension(e.FileName).Equals(fileExtension, StringComparison.InvariantCultureIgnoreCase));
+            var logoData = new MemoryStream();
+            logoFile.Extract(logoData);
+            return logoData;
+        }
+
+        private static byte[] ReadFully(Stream input)
+        {
+            // Provided by Jon Skeet: http://stackoverflow.com/a/221941/328193
+            var buffer = new byte[16 * 1024];
+            using (var ms = new MemoryStream())
+            {
+                int read;
+                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                    ms.Write(buffer, 0, read);
+                return ms.ToArray();
+            }
         }
     }
 }
