36b653f5878908befc8f9a3538cec1e48b742c97
dpdonahue <dpdonahue@gmail.com>
2014-03-10 15:40:03 -0400

    Added some rudimentary image file validation for branding images, but I'm sure we'll want something better than this.

--- OrgPortalServer/Controllers/AdminController.cs
+++ OrgPortalServer/Controllers/AdminController.cs
@@ -2,6 +2,8 @@
 using OrgPortal.Domain.Extensions;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -18,15 +20,35 @@ namespace OrgPortalServer.Controllers
         [HttpPost]
         public ActionResult Branding(string name, string primaryColor, string secondaryColor, HttpPostedFileBase logo, HttpPostedFileBase header)
         {
+            // TODO: Improve overall model binding and use better error reporting in the UI
+            if (!IsValid(logo, 200, 60) || !IsValid(header, 1366, 48))
+            {
+                TempData["WarningMessage"] = "Invalid image file.";
+                return RedirectToAction("Index");
+            }
+
             Configuration.BrandingName = name;
             Configuration.BrandingPrimaryColor = primaryColor;
             Configuration.BrandingSecondaryColor = secondaryColor;
             if (logo != null)
                 Configuration.BrandingLogo = logo.InputStream.ReadFully();
             if (header != null)
-                Configuration.BrandingHeader = logo.InputStream.ReadFully();
+                Configuration.BrandingHeader = header.InputStream.ReadFully();
             TempData["WarningMessage"] = "Branding saved.";
             return RedirectToAction("Index");
+        }
+
+        private bool IsValid(HttpPostedFileBase imageFile, int width, int height)
+        {
+            if (imageFile == null)
+                return true;
+            if (!imageFile.ContentType.Equals("image/png", StringComparison.InvariantCultureIgnoreCase))
+                return false;
+            using (Image image = Image.FromStream(imageFile.InputStream, true, true))
+                if (image.Width != width || image.Height != height)
+                    return false;
+            imageFile.InputStream.Position = 0;
+            return true;
         }
 	}
 }
