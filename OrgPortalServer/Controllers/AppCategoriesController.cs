4e6775b732918e57c8acc25d99a2c13cb310d69b
dpdonahue <dpdonahue@gmail.com>
2014-03-29 19:32:20 -0400

    Added API calls for category info.  Definitely need to clean up and consolidate the controllers a lot, as well as the overall data access.  But for now "working" is better than "perfect."

---
+++ OrgPortalServer/Controllers/AppCategoriesController.cs
@@ -0,0 +1,50 @@
+﻿using OrgPortal.Domain;
+using OrgPortal.Domain.Repositories;
+using OrgPortalServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OrgPortalServer.Controllers
+{
+    // TODO: Too many controllers.  Start with re-naming the API ones, then combine the less-important MVC ones into one or two where naming isn't a big deal.
+    public class AppCategoriesController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<CategoryInfo> Get()
+        {
+            return IoCContainerFactory.Current.GetInstance<CategoryRepository>().Categories.ToList()
+                .Select(c => new CategoryInfo { ID = c.ID, Name = c.Name });
+        }
+
+        // GET api/<controller>/5
+        public CategoryInfo Get(int id)
+        {
+            return IoCContainerFactory.Current.GetInstance<CategoryRepository>().Categories.ToList()
+                .Select(c => new CategoryInfo
+                {
+                    ID = c.ID,
+                    Name = c.Name,
+                    Apps = IoCContainerFactory.Current.GetInstance<ApplicationRepository>().Applications
+                        .Where(a => a.CategoryID == id)
+                        .ToList()
+                        .Select(a =>
+                        new AppInfo
+                        {
+                            Name = a.Name,
+                            PackageFamilyName = a.PackageFamilyName,
+                            Description = a.Description,
+                            Version = a.Version,
+                            InstallMode = a.InstallMode,
+                            Category = a.Category.Name,
+                            DateAdded = a.DateAdded,
+                            BackgroundColor = a.BackgroundColor
+                        })
+                })
+                .Single(c => c.ID == id);
+        }
+    }
+}
